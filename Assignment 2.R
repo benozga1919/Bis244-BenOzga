2c469f4efb528fbe49b8e98ae0953f2c7ca469b2
Ben Ozga <bao223@lehigh.edu>
2021-09-23 20:29:19 -0400

    assignment

---
+++ Assignment 2.R
@@ -0,0 +1,10 @@
+install.packages('dplyr')
+install.packages('data.table')
+library(dplyr)
+library('data.table')
+
+df <- fread('us-states.csv')
+df <- mutate(df, incr_cases = c(NA,diff(df$cases)))
+df <- mutate(df, incr_deaths = c(NA, diff(df$deaths)))
+
+sd(df$incr_cases, na.rm = TRUE)
